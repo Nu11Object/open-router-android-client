44e6ada5990b4b1c92bd76998f3fb2626571b31a
Nu11Object <null.object@vk.com>
2025-12-27 20:35:05 +0400

    build(deps): update dependencies and plugins

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,13 +28,18 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
+
     buildFeatures {
         viewBinding = true
         buildConfig = true
